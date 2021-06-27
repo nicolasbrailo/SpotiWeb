98356087562452f58ba928c82377d54e2d850c90
Nico Brailovsky <nicolasbrailo@gmail.com>
2021-06-27 13:10:51 +0100

    .

--- webapp/app.js
+++ webapp/app.js
@@ -6,11 +6,15 @@ function wget(url, cb, h={}) {
       headers: h,
       url: url,
       success: cb,
-      error: (_,e,ee) => console.log(`Error fetching ${url}: ${e}, ${ee}`),
+      error: console.error,
     });
 }
 
 class LocalStorageManager {
+  constructor(max_cache_age_secs) {
+    this.max_cache_age_secs = max_cache_age_secs;
+  }
+
   get(key, default_val) {
     try {
       const v = JSON.parse(localStorage.getItem(key));
@@ -23,40 +27,40 @@ class LocalStorageManager {
   save(key, val) {
     localStorage.setItem(key, JSON.stringify(val));
   }
+
+  cacheGet(key, cb_if_none) {
+    const last_update = this.get(`cache_age_for_${key}`, 0);
+    const age = Date.now() - last_update;
+    const cache_is_old = (age > 1000 * this.max_cache_age_secs);
+    if (cache_is_old) return null;
+    return this.get(key, null);
+  }
+
+  cacheSave(key, val) {
+    this.save(`cache_age_for_${key}`, Date.now());
+    this.save(key, val);
+  }
 };
 
 class CollectionManager {
-  constructor(storage, max_cache_age_secs) {
+  constructor(storage) {
     this.storage = storage;
-    this.max_cache_age_secs = max_cache_age_secs;
   }
 
   fetch(cb) {
     wget("/api/fetch_all", col => {
       col.genres.sort();
       $.each(col.artists_by_genre, (_,arts) => { arts.sort(); });
-      this.storage.save('collection', col);
-      this.storage.save('collection_last_update', Date.now());
+      this.storage.cacheSave('collection', col);
       cb(col);
     });
   }
 
   cachedFetch(cb) {
-    const last_update = this.storage.get('collection_last_update', 0);
-    const col_age = Date.now() - last_update;
-    const cache_is_old = (col_age > 1000 * this.max_cache_age_secs);
-    if (cache_is_old) {
-      console.log("Collection cache is old, refetching");
-      return this.fetch(cb);
-    }
-
-    const col = this.storage.get('collection', null);
-    if (col && col.genres && Array.isArray(col.genres)) {
-      cb(col);
-    } else {
-      console.log("Collection cache is not valid, refetching");
-      return this.fetch(cb);
-    }
+    const col = this.storage.cacheGet('collection');
+    if (col) return cb(col);
+    console.log("Collection cache not valid, refetching");
+    return this.fetch(cb);
   }
 };
 
@@ -69,7 +73,7 @@ class RecentlyPlayed {
   get() {
     var lastPlayed = this.storage.get('lastPlayed', []);
     if (!Array.isArray(lastPlayed)) return [];
-    return lastPlayed;
+    return lastPlayed.reverse();
   }
 
   add(art) {
@@ -85,19 +89,14 @@ class RecentlyPlayed {
 };
 
 class UI_Builder {
-  static onArtClickCb = a => console.log(a);
-  static onArtExpandViewClickCb = a => console.log(a);
-
-  constructor(recentlyPlayed, onArtClickCb) {
+  static self = null;
+  constructor(recentlyPlayed) {
     this.recentlyPlayed = recentlyPlayed;
-  }
-
-  onArtClicked(cb) {
-    UI_Builder.onArtClickCb = cb;
-  }
-
-  onArtExpandViewClicked(cb) {
-    UI_Builder.onArtExpandViewClickCb = cb;
+    this.onArtistClickedCb = console.log;
+    this.onArtistExpandClickedCb = console.log;
+    this.onAlbumClickedCb = console.log;
+    this.known_albums = {};
+    UI_Builder.self = this;
   }
 
   setCollection(col) {
@@ -105,7 +104,26 @@ class UI_Builder {
   }
 
   buildGenreHref(gen) {
-    return gen.replace(' ', '-');
+    return gen.replaceAll(' ', '-');
+  }
+
+  onAlbumClicked(cb) {this.onAlbumClickedCb = cb; }
+  static trampolineOnAlbumClicked(art_name, album_id) {
+    const art_obj = UI_Builder.self.collection.artists[art_name];
+    const album_obj = UI_Builder.self.known_albums[album_id];
+    UI_Builder.self.onAlbumClickedCb(art_obj, album_obj);
+  }
+
+  onToggleArtistExtendedViewClicked(cb) { this.onArtistExpandClickedCb = cb; }
+  static trampolineToggleArtistExtendedView(src_id, art_name) {
+    const art_obj = UI_Builder.self.collection.artists[art_name];
+    UI_Builder.self.onArtistExpandClickedCb(src_id, art_obj);
+  }
+
+  onArtistClicked(cb) { this.onArtistClickedCb = cb; }
+  static trampolineOnArtistClicked(art) {
+    const art_obj = UI_Builder.self.collection.artists[art];
+    UI_Builder.self.onArtistClickedCb(art_obj);
   }
 
   buildSingleArt(art, src_id='') {
@@ -113,10 +131,11 @@ class UI_Builder {
     if (!art_info) {
       return `<li><img src="https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG"/>${art}</li>`;
     } else {
+      const imgurl = art_info.img? art_info.img : "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG";
       return `<li id='art${src_id}_node_${art_info.id}'>
-              <div class="expandView" onclick='UI_Builder.onArtExpandViewClickCb("${src_id}", "${art}", "${art_info.id}", "${art_info.uri}")'>&#9660;</div>
-              <a href='${art_info.uri}' onclick='UI_Builder.onArtClickCb("${art}")'>
-                <img src='${art_info.img}'/>
+              <div class="expandView" onclick='UI_Builder.trampolineToggleArtistExtendedView("${src_id}", "${art}")'>&#9660;</div>
+              <a href='javascript:' onclick='UI_Builder.trampolineOnArtistClicked("${art}")'>
+                <img src='${imgurl}'/>
                 ${art}
               </a>
               <div id='art${src_id}_extended_info_${art_info.id}'>...</div>
@@ -160,7 +179,17 @@ class UI_Builder {
     return `<h2>Recently played</h2><ul class='arts'>${lastPlayedUi}</ul>`;
   }
 
-  buildAlbumList(art, albums) {
+  buildExtendedView(art_name, albums) {
+    const art = this.collection.artists[art_name];
+    const genres = art.genres
+                    .filter(gen => this.collection.genres.includes(gen))
+                    .map(gen => `<li><a href=#${this.buildGenreHref(gen)}>${gen}</a><li>`)
+                    .join('');
+    const genres_view = (genres.length == 0)? '' : `<p>More: <ul class="genresIdx">${genres}</ul></p>`
+    return genres_view + this.buildAlbumList(art_name, albums);
+  }
+
+  buildAlbumList(art_name, albums) {
     const imgClosestTo200 = imgs => {
       if (!imgs || !Array.isArray(imgs) || imgs.length == 0) {
         return "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG";
@@ -183,8 +212,11 @@ class UI_Builder {
       if (seen_albums.includes(seen)) return '';
       seen_albums.push(seen);
 
+      // Save this album to look it up again in the onClick callback
+      this.known_albums[album.id] = album;
+
       return `<li class='album_info'>
-                <a href='${album.uri}' onclick='UI_Builder.onArtClickCb("${art}")'>
+                <a href='javascript:' onclick='UI_Builder.trampolineOnAlbumClicked("${art_name}", "${album.id}")'>
                   <img src='${imgClosestTo200(album.images)}'/>
                   ${(new Date(album.release_date)).getFullYear()} - ${album.name}
                 </a>
@@ -195,33 +227,49 @@ class UI_Builder {
 };
 
 class SpotifyProxy {
-  fetchAlbumsFor(id, cb) {
+  SpotifyProxy(cache) {
+    this.cache = cache;
+  }
+
+  _spApi(action, url, data) {
+    const promise = $.Deferred();
     wget("/api/get_tok", auth => {
-      $.ajax({
-          type: 'GET',
+      const req = {
+          type: action,
           dataType: 'json',
           headers: auth,
-          success: resp => {
-            if (resp.items > 50) {
-              console.error(`${id} requires pagination. Not implemented`);
-            }
-            cb(resp.items);
-          },
-          error: (_,e,ee) => console.log(`Error fetching ${url}: ${e}, ${ee}`),
-          url: `https://api.spotify.com/v1/artists/${id}/albums?limit=50&include_groups=album`,
-        });
+          success: promise.resolve,
+          error: console.error,
+          url: 'https://api.spotify.com/v1/' + url,
+        }
+      if (data) req['data'] = JSON.stringify(data);
+      $.ajax(req);
     });
+    return promise;
+  }
+
+  fetchAlbumsFor(artist_id, cb) {
+    this._spApi('GET', `artists/${artist_id}/albums?limit=50&include_groups=album`).then( resp => {
+      if (resp.items > 50) {
+        console.error(`Albums for artist ${artist_id} requires pagination. Not implemented`);
+      }
+      cb(resp.items);
+    });
+  }
+
+  play(obj) {
+    return this._spApi('PUT', 'me/player/play', {'context_uri': obj.uri});
   }
 };
 
 const HISTORY_CNT_LAST_ARTS_PLAYED = 10;
 const MAX_CACHE_AGE_SECS = 60 * 60 * 24 * 3;
 
-const storage = new LocalStorageManager();
-const collection = new CollectionManager(storage, MAX_CACHE_AGE_SECS);
+const storage = new LocalStorageManager(MAX_CACHE_AGE_SECS);
+const collection = new CollectionManager(storage);
 const recentlyPlayed = new RecentlyPlayed(storage, HISTORY_CNT_LAST_ARTS_PLAYED);
 const ui = new UI_Builder(recentlyPlayed);
-const sp = new SpotifyProxy();
+const sp = new SpotifyProxy(storage);
 
 function rebuildRecentlyPlayed() {
   $('#recently_played').html(ui.buildRecentlyPlayed());
@@ -233,16 +281,27 @@ function rebuildUI() {
   rebuildRecentlyPlayed();
 }
 
-ui.onArtClicked(art => {
-  recentlyPlayed.add(art);
+ui.onArtistClicked(art => {
+  recentlyPlayed.add(art.name);
   rebuildRecentlyPlayed();
+  sp.play(art).then(_ => {
+    window.location = art.uri;
+  });
 });
 
-ui.onArtExpandViewClicked((src, art, id, uri) => {
-  const ext_view = ui.toggleExtendedView(src, id);
+ui.onAlbumClicked((art,album) => {
+  recentlyPlayed.add(art.name);
+  rebuildRecentlyPlayed();
+  sp.play(album).then(_ => {
+    window.location = album.uri;
+  });
+});
+
+ui.onToggleArtistExtendedViewClicked((src, art) => {
+  const ext_view = ui.toggleExtendedView(src, art.id);
   if (ext_view) {
-    sp.fetchAlbumsFor(id, artAlbs => {
-      ext_view.innerHTML = ui.buildAlbumList(art, artAlbs);
+    sp.fetchAlbumsFor(art.id, artAlbs => {
+      ext_view.innerHTML = ui.buildExtendedView(art.name, artAlbs);
     });
   }
 });
